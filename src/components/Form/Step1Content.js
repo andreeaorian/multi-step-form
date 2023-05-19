247f06093e58b2be142fa8dd83722a8d2b5c000a
andreea-orian <andreea.orian@dotwhite.com>
2023-05-19 16:38:08 +0300

    Add placeholder to all inputs

--- src/components/Form/Step1Content.js
+++ src/components/Form/Step1Content.js
@@ -3,12 +3,24 @@ export function Step1Content() {
 		<form className="step1">
 			<div className="group">
 				<label htmlFor="name">Name</label> <br />
-				<input type="text" id="name" name="name" required />
+				<input
+					type="text"
+					id="name"
+					name="name"
+					placeholder="e.g. Stephen King"
+					required
+				/>
 			</div>
 
 			<div className="group">
 				<label htmlFor="email">Email Address</label> <br />
-				<input type="email" id="email" name="email" required />
+				<input
+					type="email"
+					id="email"
+					name="email"
+					placeholder="e.g. stephenking@lorem.com"
+					required
+				/>
 			</div>
 
 			<div className="group">

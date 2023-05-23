d671b85b5e9b944a6c383cd59cdfc441a0bc6e25
andreea-orian <andreea.orian@dotwhite.com>
2023-05-23 12:57:08 +0300

    Select billing plan and save it in context

--- src/components/Form/planBox.js
+++ src/components/Form/planBox.js
@@ -1,11 +1,18 @@
-export function PlanBox({ icon: Icon, title, price, isYearly }) {
+export function PlanBox({
+	icon: Icon,
+	title,
+	price,
+	isYearly,
+	isSelected,
+	select,
+}) {
 	return (
-		<button className="plan-box">
+		<button
+			className={`plan-box ${isSelected ? "selected" : ""}`}
+			onClick={select}>
 			<Icon width={40} height={40} className="plan-icon" />
 			<div className="plan-title">{title}</div>
-			<div className="plan-price">{`$${price}/${
-				isYearly ? "/yr" : "/mo"
-			}`}</div>
+			<div className="plan-price">{`$${price}${isYearly ? "/yr" : "/mo"}`}</div>
 			<div className="plan-discount" hidden={!isYearly}>
 				2 months free
 			</div>
--- src/components/Form/step2Content.js
+++ src/components/Form/step2Content.js
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { PlanBox } from "./planBox";
 import { ReactComponent as ArcadeIcon } from "../../assets/img/icon-arcade.svg";
 import { ReactComponent as AdvancedIcon } from "../../assets/img/icon-advanced.svg";
 import { ReactComponent as ProIcon } from "../../assets/img/icon-pro.svg";
+import { FormDataContext } from "../../context/formDataContext";
+
+const arcadePlan = {
+	title: "Arcade",
+	monthlyPrice: 9,
+	yearlyPrice: 90,
+};
+const advancedPlan = {
+	title: "Advanced",
+	monthlyPrice: 12,
+	yearlyPrice: 120,
+};
+const proPlan = {
+	title: "Pro",
+	monthlyPrice: 15,
+	yearlyPrice: 150,
+};
+const allPlans = [arcadePlan, advancedPlan, proPlan];
 
 export function Step2Content() {
-	const [isYearlyBilling, setIsYearlyBilling] = useState(false);
+	const { planType, yearlyBilling, setBillingPlan } =
+		useContext(FormDataContext);
+
 	const changeBilling = () => {
-		setIsYearlyBilling((prevState) => {
-			return !prevState;
+		setBillingPlan({ yearlyBilling: !yearlyBilling });
+	};
+
+	const choosePlan = (title) => {
+		const selectedPlan = allPlans.find((x) => x.title === title);
+		const price = yearlyBilling
+			? selectedPlan.yearlyPrice
+			: selectedPlan.monthlyPrice;
+
+		setBillingPlan({
+			planType: selectedPlan.title,
+			price: price,
 		});
 	};
 
@@ -17,39 +47,48 @@ export function Step2Content() {
 			<div className="step2-plans">
 				<PlanBox
 					icon={ArcadeIcon}
-					title="Arcade"
-					price={90}
-					isYearly={isYearlyBilling}
+					title={arcadePlan.title}
+					price={
+						yearlyBilling ? arcadePlan.yearlyPrice : arcadePlan.monthlyPrice
+					}
+					isYearly={yearlyBilling}
+					isSelected={planType === arcadePlan.title}
+					select={() => choosePlan(arcadePlan.title)}
 				/>
 				<PlanBox
 					icon={AdvancedIcon}
-					title="Advanced"
-					price={120}
-					isYearly={isYearlyBilling}
+					title={advancedPlan.title}
+					price={
+						yearlyBilling ? advancedPlan.yearlyPrice : advancedPlan.monthlyPrice
+					}
+					isYearly={yearlyBilling}
+					isSelected={planType === advancedPlan.title}
+					select={() => choosePlan(advancedPlan.title)}
 				/>
 				<PlanBox
 					icon={ProIcon}
-					title="Pro"
-					price={150}
-					isYearly={isYearlyBilling}
+					title={proPlan.title}
+					price={yearlyBilling ? proPlan.yearlyPrice : proPlan.monthlyPrice}
+					isYearly={yearlyBilling}
+					isSelected={planType === proPlan.title}
+					select={() => choosePlan(proPlan.title)}
 				/>
 			</div>
 			<div className="step2-billing">
 				<label htmlFor="billingType" className="billing-toggle">
 					<span
-						className={`billing-monthly ${isYearlyBilling ? "gray" : "blue"}`}>
+						className={`billing-monthly ${yearlyBilling ? "gray" : "blue"}`}>
 						Montly
 					</span>
 					<input
 						className="billing-toggle-input"
 						type="checkbox"
 						id="billingType"
-						value={isYearlyBilling}
+						value={yearlyBilling}
 						onChange={changeBilling}
 					/>
 					<span className="billing-toggle-handler" />
-					<span
-						className={`billing-yearly ${isYearlyBilling ? "blue" : "gray"}`}>
+					<span className={`billing-yearly ${yearlyBilling ? "blue" : "gray"}`}>
 						Yearly
 					</span>
 				</label>

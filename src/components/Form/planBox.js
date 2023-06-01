1de6807a2d71a15ba3f7e78c2c1b7ac7313a80d5
andreea-orian <andreea.orian@dotwhite.com>
2023-06-01 18:05:37 +0300

    Responsive design for step 2

--- src/components/Form/planBox.js
+++ src/components/Form/planBox.js
@@ -1,3 +1,5 @@
+import useDisplaySize from "../../hooks/useDisplaySize";
+
 export default function PlanBox({
 	icon: Icon,
 	title,
@@ -6,15 +8,22 @@ export default function PlanBox({
 	isSelected,
 	select,
 }) {
+	const isMobile = useDisplaySize();
+	const imageSize = isMobile ? 30 : 40;
+
 	return (
 		<button
 			className={`plan-box ${isSelected ? "selected" : ""}`}
 			onClick={select}>
-			<Icon width={40} height={40} className="plan-icon" />
-			<div className="plan-title">{title}</div>
-			<div className="plan-price">{`$${price}${isYearly ? "/yr" : "/mo"}`}</div>
-			<div className="plan-discount" hidden={!isYearly}>
-				2 months free
+			<Icon width={imageSize} height={imageSize} className="plan-icon" />
+			<div className="plan-content">
+				<div className="plan-title">{title}</div>
+				<div className="plan-price">{`$${price}${
+					isYearly ? "/yr" : "/mo"
+				}`}</div>
+				<div className="plan-discount" hidden={!isYearly}>
+					2 months free
+				</div>
 			</div>
 		</button>
 	);

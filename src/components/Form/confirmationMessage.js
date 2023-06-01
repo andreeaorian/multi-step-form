746ae5781ecdf467000173bbcab273a32a6476ba
andreea-orian <andreea.orian@dotwhite.com>
2023-06-01 19:22:43 +0300

    Responsive design for step 4 and confirmation message

--- src/components/Form/confirmationMessage.js
+++ src/components/Form/confirmationMessage.js
@@ -1,14 +1,17 @@
 import { useContext } from "react";
 import { FormDataContext } from "../../context/formDataContext";
+import useDisplaySize from "../../hooks/useDisplaySize";
 
 import { ReactComponent as ThankYouIcon } from "../../assets/img/icon-thank-you.svg";
 
 export default function ConfirmationMessage() {
 	const { isConfirmed } = useContext(FormDataContext);
+	const isMobile = useDisplaySize();
+	const imageSize = isMobile ? 40 : 80;
 
 	return (
 		<div className={`confirmation-message ${isConfirmed ? "" : "hidden"}`}>
-			<ThankYouIcon width={80} height={80} />
+			<ThankYouIcon width={imageSize} height={imageSize} />
 			<h1 className="confirmation-title"> Thank you!</h1>
 			<div className="confirmation-details">
 				Thanks for confirming your subscription! We hope you have fun using our
